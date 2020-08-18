d879c36d17728fa0629f0e17331ec2d89a005633
Nick Schoess <schoessnick@gmail.com>
2020-08-18 18:12:28 -0500

    placed navigation inside home js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Home from "./Components/Home.js";
-import Navbar from './Components/Navbar.js';
 
 function App() {
   return (
     <div>
     <Home></Home>
-    <Navbar></Navbar>
     </div>
   );
 }
