1533921f0bd1757495ce185998f5aad044cbe10b
nick <schoessnick@gmail.com>
2020-08-18 15:25:59 -0500

    debugging navbar

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import Navbar from "./Navbar";
 
 class Home extends Component {
     render() {
         return(
             <div>
-                <h1>Hello kids!</h1>
+                <h1>Hello World!</h1>
             </div>
         )
     }
