0e3cea579ff248570cc2ed41daa1292bff31e1b9
Nick Schoess <schoessnick@gmail.com>
2020-08-20 18:16:59 -0500

    began building page. built data in json file, added background image

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -3,8 +3,13 @@ import Navigation from "../Navigation/Navigation";
 import Foot from "../Foot/Foot";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Portfolio.css";
+import data from "../../assets/seed/data.json"
 
 class Portfolio extends Component {
+    state = {
+        data
+    }
+
     render() {
         return(
             <div>
