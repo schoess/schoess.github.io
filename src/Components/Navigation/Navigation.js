1b4419fd252682131ed178f32f5ee4de8858b638
nick <schoessnick@gmail.com>
2020-08-20 09:51:51 -0500

    edited nav bar to include logo

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,30 +1,24 @@
 import React, { Component } from "react";
 import Nav from "react-bootstrap/Nav";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import FormControl from "react-bootstrap/FormControl";
 import Navbar from "react-bootstrap/Navbar";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from "../../assets/images/schoesslogo_white.png"
+import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "../../assets/images/schoesslogo_white.png";
+import "./Navigation.css";
 
 
 class Navigation extends Component {
     render() {
         return (
             <div>
-                <Navbar bg="dark" variant="dark">
+                <Navbar bg="dark" id="bg-color" variant="dark">
                     <Navbar.Brand href="#home">
-                        <img className="nav-logo" src={ logo } alt= "Schoess logo" style={{width: 100}} />
+                        <img className="nav-logo" src={ logo } alt= "Schoess logo" />
                     </Navbar.Brand>
                         <Nav className="mr-auto">
                             <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#features">Features</Nav.Link>
-                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                            <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+                            <Nav.Link href="#contact">Contact</Nav.Link>
                         </Nav>
-                    <Form inline>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                    <Button variant="outline-info">Search</Button>
-                    </Form>
                 </Navbar>
             </div>
         )
