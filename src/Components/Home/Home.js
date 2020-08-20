5969f6f24c599cb41f75a0cca2b71f3d247f4a73
Nick Schoess <schoessnick@gmail.com>
2020-08-20 11:57:23 -0500

    added footer and changed some colors

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -7,25 +7,26 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import logo from "../../assets/images/IMG_5529.jpg";
+import Foot from "../Foot/Foot";
 
 class Home extends Component {
     render() {
         return(
         <div>
             <Helmet>
-                <style>{'body { background-color: #38515b; }'}</style>
+                <style>{'body { background-color: #25252a; }'}</style>
             </Helmet>
-            <div>
-                <Navigation></Navigation>
-            </div>
 
-            <Container classname="container">
+            <Navigation />
+
+            <Container className="container">
                 <Row>
                     <Col xs={6} md={4}><img className="headshot" src={ logo } alt= "Headshot of Nick" /></Col>
-                    <Col xs={12} md={8}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Col>
+                    <Col className="body-text" xs={12} md={8}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Col>
                 </Row>
             </Container>
 
+            <Foot />
         </div>
         )
     }
