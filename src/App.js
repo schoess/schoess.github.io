e08a805a53d8912371cad0ff7175d5539c7d928f
Nick Schoess <schoessnick@gmail.com>
2020-08-20 12:23:25 -0500

    added react-router syntax

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import Home from "./Components/Home/Home";
+import Contact from "./Components/Contact/Contact";
+import Portfolio from "./Components/Portfolio/Portfolio";
+import {
+  Route,
+  Switch
+} from 'react-router-dom';
 
 function App() {
   return (
     <div>
-    <Home></Home>
+    <Switch>
+            <Route exact path="/" component= { Home } />
+            <Route exact path="/portfolio" component={ Portfolio } />
+            <Route exact path="/contact" component={ Contact } />
+          </Switch>
     </div>
   );
 }
